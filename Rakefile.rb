413c82af1a23f9db49004f8797a40f28f8d692f6
John Lees-Miller <jdleesmiller@gmail.com>
2010-12-07 10:54:45 +0000

    doc release task

--- Rakefile.rb
+++ Rakefile.rb
@@ -18,10 +18,8 @@ task :default => :test
 CLOBBER << 'test/my_new_gem_base'
 CLOBBER << 'test/my_new_gem_full'
 
-=begin
 desc "docs to rubyforge"
-task :publish_docs => :rdoc_sh do
+task :publish_docs => :yard do
   sh "rsync --archive --delete --verbose yard/* "\
      " jdleesmiller@rubyforge.org:/var/www/gforge-projects/gemma"
 end
-=end
