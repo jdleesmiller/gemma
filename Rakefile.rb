ea3da3745607ad4d31319581f7afe9b96755ccd6
John Lees-Miller <jdleesmiller@gmail.com>
2011-09-05 22:37:01 +0100

    added some rubyforge notes

--- Rakefile.rb
+++ Rakefile.rb
@@ -11,3 +11,9 @@ task :publish_docs => :yard do
   sh "rsync --archive --delete --verbose yard/* "\
      " jdleesmiller@rubyforge.org:/var/www/gforge-projects/gemma"
 end
+
+# note: to release:
+# gem install rubyforge
+# rubyforge setup
+# rubyforge config
+# rubyforge add_release gemma gemma 2.0.0 pkg/gemma-2.0.0.gem
