132c04ad3330e189764752583d02da0c6cc4cefd
John Lees-Miller <jdleesmiller@gmail.com>
2017-03-10 08:05:31 +0000

    Bump version

--- lib/gemma/version.rb
+++ lib/gemma/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Gemma
-  VERSION_MAJOR = 4
-  VERSION_MINOR = 1
+  VERSION_MAJOR = 5
+  VERSION_MINOR = 0
   VERSION_PATCH = 0
   VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
 end
